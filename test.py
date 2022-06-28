54efa61424c609d53ced3207a42537513daae789
a-hilhorst <antoine.hilhorst@uclouvain.be>
2022-06-28 16:59:48 +0200

    * now handles input of type n_im = 10,15x20,30

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import PySimpleGUI as Sg
+import os
+
+ptListIn = os.getcwd() + '\IN_2_4_2.TXT'
+ptListOut = os.getcwd() + '\IN_2_4_2_OUT.TXT'
 
 layout = [
     [Sg.Text('# images of each series: ',
@@ -29,6 +33,38 @@ while True:
             event, values = window.read()
             continue
         break
+
+    with open(ptListIn, 'r') as pl:
+        pll = pl.readlines()
+
+        v_x = []
+        v_y = []
+        v_z = []
+        v_t = []
+        v_r = []
+        v_wd = []
+        for i in range(4, len(pll)):
+            tmp = pll[i].split(',')
+            v_x.append(float(tmp[1]))
+            v_y.append(float(tmp[2]))
+            v_z.append(float(tmp[3]))
+            v_t.append(float(tmp[4]))
+            v_r.append(float(tmp[5]))
+            v_wd.append(float(tmp[8][0:-1]))
+
+    lnim = len([i for j in n_im for i in j])
+    totim = np.sum([np.prod(j) for j in n_im])
+
+    if len(v_x) == 2*lnim:
+        print('OK')
+
+        with open(ptListOut, 'w') as pl:
+            pl.write('Leo Points List\n')
+            pl.write('Absolute\n')
+            pl.write('Label,X,Y,Z,T,R,M,Mag,WD\n')
+            pl.write(str(totim + 1) + '\n')
+    else:
+        Sg.Popup(str(len(v_x)) + ' ' + str(2*lnim))
 
     if event in 'T-E-S-T':
         Sg.Popup(n_im)
